3872b5941346bea86862f9d4b651b0f269474ac9
Chuck Preslar <chuckpreslar@gmail.com>
2014-09-15 17:12:19 -0600

    Add namespace functionality.

--- dispatcher.go
+++ dispatcher.go
@@ -7,24 +7,79 @@ import (
 )
 
 import (
+	"github.com/chuckpreslar/dispatcher/headers"
 	"github.com/chuckpreslar/dispatcher/methods"
 	"github.com/chuckpreslar/dispatcher/statuses"
 )
 
+// Namespace ...
+type Namespace struct {
+	prefix     []string
+	dispatcher *Dispatcher
+}
+
+// Get ...
+func (n *Namespace) Get(url string, handlers ...http.Handler) *Namespace {
+	url = fmt.Sprintf("%s/%s", strings.Join(n.prefix, "/"), strings.TrimLeft(url, "/"))
+	n.dispatcher.Route(methods.Get, url, handlers...)
+	return n
+}
+
+// Put ...
+func (n *Namespace) Put(url string, handlers ...http.Handler) *Namespace {
+	url = fmt.Sprintf("%s/%s", strings.Join(n.prefix, "/"), strings.TrimLeft(url, "/"))
+	n.dispatcher.Route(methods.Put, url, handlers...)
+	return n
+}
+
+// Post ...
+func (n *Namespace) Post(url string, handlers ...http.Handler) *Namespace {
+	url = fmt.Sprintf("%s/%s", strings.Join(n.prefix, "/"), strings.TrimLeft(url, "/"))
+	n.dispatcher.Route(methods.Post, url, handlers...)
+	return n
+}
+
+// Patch ...
+func (n *Namespace) Patch(url string, handlers ...http.Handler) *Namespace {
+	url = fmt.Sprintf("%s/%s", strings.Join(n.prefix, "/"), strings.TrimLeft(url, "/"))
+	n.dispatcher.Route(methods.Patch, url, handlers...)
+	return n
+}
+
+// Delete ...
+func (n *Namespace) Delete(url string, handlers ...http.Handler) *Namespace {
+	url = fmt.Sprintf("%s/%s", strings.Join(n.prefix, "/"), strings.TrimLeft(url, "/"))
+	n.dispatcher.Route(methods.Delete, url, handlers...)
+	return n
+}
+
+// Namespace ...
+func (n *Namespace) Namespace(prefix string) *Namespace {
+	prefix = strings.TrimSuffix(strings.TrimPrefix(prefix, "/"), "/")
+	return &Namespace{prefix: append(n.prefix, prefix), dispatcher: n.dispatcher}
+}
+
 // Dispatcher ...
 type Dispatcher struct {
-	router *Router
+	router     *Router
+	middleware []http.Handler
+}
+
+// Namespace ...
+func (d *Dispatcher) Namespace(prefix string) *Namespace {
+	return &Namespace{prefix: []string{strings.TrimRight(prefix, "/")}, dispatcher: d}
 }
 
 // Route ...
-func (d *Dispatcher) Route(method, url string, handler http.Handler) *Dispatcher {
+func (d *Dispatcher) Route(method, url string, handlers ...http.Handler) *Dispatcher {
 	method = strings.ToUpper(method)
+
 	var nodes = d.router.Route(url)
 
 	for i := 0; i < len(nodes); i++ {
 		var node = nodes[i]
 
-		node.handlers[method] = append(node.handlers[method], handler)
+		node.handlers[method] = append(node.handlers[method], handlers...)
 
 		if -1 == strings.Index(strings.Join(node.methods, ""), method) {
 			node.methods = append(node.methods, method)
@@ -35,28 +90,28 @@ func (d *Dispatcher) Route(method, url string, handler http.Handler) *Dispatcher
 }
 
 // Get ...
-func (d *Dispatcher) Get(url string, handler http.Handler) *Dispatcher {
-	return d.Route(methods.Get, url, handler)
+func (d *Dispatcher) Get(url string, handlers ...http.Handler) *Dispatcher {
+	return d.Route(methods.Get, url, handlers...)
 }
 
 // Put ...
-func (d *Dispatcher) Put(url string, handler http.Handler) *Dispatcher {
-	return d.Route(methods.Put, url, handler)
+func (d *Dispatcher) Put(url string, handlers ...http.Handler) *Dispatcher {
+	return d.Route(methods.Put, url, handlers...)
 }
 
 // Post ...
-func (d *Dispatcher) Post(url string, handler http.Handler) *Dispatcher {
-	return d.Route(methods.Post, url, handler)
+func (d *Dispatcher) Post(url string, handlers ...http.Handler) *Dispatcher {
+	return d.Route(methods.Post, url, handlers...)
 }
 
 // Patch ...
-func (d *Dispatcher) Patch(url string, handler http.Handler) *Dispatcher {
-	return d.Route(methods.Patch, url, handler)
+func (d *Dispatcher) Patch(url string, handlers ...http.Handler) *Dispatcher {
+	return d.Route(methods.Patch, url, handlers...)
 }
 
 // Delete ...
-func (d *Dispatcher) Delete(url string, handler http.Handler) *Dispatcher {
-	return d.Route(methods.Delete, url, handler)
+func (d *Dispatcher) Delete(url string, handlers ...http.Handler) *Dispatcher {
+	return d.Route(methods.Delete, url, handlers...)
 }
 
 // ServeHTTP ...
@@ -73,11 +128,11 @@ func (d *Dispatcher) ServeHTTP(response http.ResponseWriter, request *http.Reque
 	var (
 		node     = match.node
 		method   = strings.ToUpper(request.Method)
-		handlers = node.handlers[method]
+		handlers = append(node.handlers[method], d.middleware...)
 	)
 
 	if methods.Options == method {
-		header.Set("Allow", strings.Join(node.methods, ","))
+		header.Set(headers.Allow, strings.Join(node.methods, ","))
 		response.WriteHeader(statuses.OK)
 		return
 	}
@@ -85,6 +140,12 @@ func (d *Dispatcher) ServeHTTP(response http.ResponseWriter, request *http.Reque
 	for i := 0; i < len(handlers); i++ {
 		handlers[i].ServeHTTP(response, request)
 	}
+}
+
+// Use ...
+func (d *Dispatcher) Use(handlers ...http.Handler) *Dispatcher {
+	d.middleware = append(d.middleware, handlers...)
+	return d
 }
 
 // Listen ...
@@ -103,6 +164,6 @@ func New() *Dispatcher {
 	root.child = make(map[interface{}]*Node)
 	router.root = root
 	dispatcher.router = router
-
+	dispatcher.middleware = make([]http.Handler, 0, 0)
 	return dispatcher
 }
